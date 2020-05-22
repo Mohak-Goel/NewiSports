473416001b260ae6089f916dfdc7af1f7b6986dc
Payel1998 <47295744+Payel1998@users.noreply.github.com>
2020-05-23 02:10:54 +0530

    event creation form

---
+++ app/src/main/java/com/example/myapplication/CreateEvent1.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateEvent1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event1);
+    }
+    private void intentCall(View view){
+        Intent homeIntent=new Intent(CreateEvent1.this,CreateEvent2.class);
+        startActivity(homeIntent);
+    }
+}
