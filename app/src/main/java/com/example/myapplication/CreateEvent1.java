fdd2e84a9329ca539a73fec80e128423a8dfcacf
Payel1998 <47295744+Payel1998@users.noreply.github.com>
2020-05-23 20:05:43 +0530

    Date&Time Picker

--- app/src/main/java/com/example/myapplication/CreateEvent1.java
+++ app/src/main/java/com/example/myapplication/CreateEvent1.java
@@ -3,20 +3,30 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class CreateEvent1 extends AppCompatActivity {
     TextView tvDate;
     EditText etDate;
+    EditText chooseTime;
+    TimePickerDialog timePickerDialog;
+
+    Calendar calendar;
+    int currentHour;
+    int currentMinute;
+    String amPm;
     DatePickerDialog.OnDateSetListener setListener;
 
     @Override
@@ -24,8 +34,34 @@ public class CreateEvent1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event1);
 
-        tvDate=findViewById(R.id.tv_date);
-        etDate=findViewById(R.id.et_date);
+        chooseTime = findViewById(R.id.etChooseTime);
+        chooseTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(CreateEvent1.this, new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        chooseTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                        //chooseTime.setText(hourOfDay + ":" + minutes + amPm);
+                    }
+                }, currentHour, currentMinute, false);
+                timePickerDialog.show();
+            }
+        });
+
+
+        tvDate = findViewById(R.id.tv_date);
+        etDate = findViewById(R.id.et_date);
 
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
@@ -35,43 +71,45 @@ public class CreateEvent1 extends AppCompatActivity {
         tvDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog datePickerDialog=new DatePickerDialog(
-                        CreateEvent1.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                    setListener,year,month,day);
-        datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        datePickerDialog.show();
-                }
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        CreateEvent1.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
 
         });
 
-       setListener=new DatePickerDialog.OnDateSetListener() {
-           @Override
-           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month=month+1;
-                String date=day+"/"+month+"/"+year;
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = day + "/" + month + "/" + year;
                 tvDate.setText(date);
-           }
-       };
-       etDate.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v){
-               DatePickerDialog datePickerDialog=new DatePickerDialog(
-                       CreateEvent1.this, new DatePickerDialog.OnDateSetListener() {
-                   @Override
-                   public void onDateSet(DatePicker view, int year, int month, int day) {
-                       month=month+1;
-                       String date=day+"/"+month+"/"+year;
-                       etDate.setText(date);
-                   }
-               },year,month,day);
-               datePickerDialog.show();
-           }
-       });
+            }
+        };
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        CreateEvent1.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
+                        etDate.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
     }
-    public void intentCall(View view){
-        Intent homeIntent=new Intent(CreateEvent1.this,CreateEvent2.class);
-        startActivity(homeIntent);
+        public void intentCall(View view){
+            Intent homeIntent = new Intent(CreateEvent1.this, CreateEvent2.class);
+            startActivity(homeIntent);
+        }
     }
 
 
-}
+
