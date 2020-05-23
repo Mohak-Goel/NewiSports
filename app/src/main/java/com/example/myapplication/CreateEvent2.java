64631d1e253b9999060bb92f57f597c729d22fd9
NischayKG <1705421@kiit.ac.in>
2020-05-23 19:42:44 +0530

    Registration Form Details Commit

--- app/src/main/java/com/example/myapplication/CreateEvent2.java
+++ app/src/main/java/com/example/myapplication/CreateEvent2.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 
 public class CreateEvent2 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class CreateEvent2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event2);
+
+        RadioButton rb;
+        rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
     }
+
+
 }
