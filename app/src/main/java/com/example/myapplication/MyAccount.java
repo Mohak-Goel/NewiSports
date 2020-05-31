880f83d14f4e05611fa2f8032fbf9d97cb829665
NischayKG <1705421@kiit.ac.in>
2020-05-31 18:55:56 +0530

    Minor Changes in My account again

--- app/src/main/java/com/example/myapplication/MyAccount.java
+++ app/src/main/java/com/example/myapplication/MyAccount.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,7 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MyAccount extends AppCompatActivity {
-    TextView un,ph,altph,utype,eid;
+    TextView un,Laddr,cityad,statead,pincodead,ph,altph,utype,eid;
     DatabaseReference reff;
     FirebaseUser user_a;
     String uid_a;
@@ -25,10 +26,11 @@ public class MyAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
-    //    n=findViewById(R.id.namedisp);
         un=findViewById(R.id.univdisp);
-    //    uroll=findViewById(R.id.rolldisp);
-    //    dob=findViewById(R.id.dobdisp);
+        Laddr=findViewById(R.id.Laddrdisp);
+        cityad=findViewById(R.id.citydisp);
+        statead=findViewById(R.id.statedisp);
+        pincodead=findViewById(R.id.pincodedisp);
         ph=findViewById(R.id.phondisp);
         altph=findViewById(R.id.altphodisp);
         utype=findViewById(R.id.userdisp);
@@ -39,16 +41,31 @@ public class MyAccount extends AppCompatActivity {
         reff.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String university_na=dataSnapshot.child("UniversityName").getValue().toString();
-                String phone_no=dataSnapshot.child("PhoneNumber").getValue().toString();
-                String Alt_phone_no=dataSnapshot.child("AlternativeNumber").getValue().toString();
-                String email_address=dataSnapshot.child("EmailAddress").getValue().toString();
-                String user_type=dataSnapshot.child("UserType").getValue().toString();
-                un.setText(university_na);
-                ph.setText(phone_no);
-                altph.setText(Alt_phone_no);
-                eid.setText(email_address);
-                utype.setText(user_type);
+                try {
+                    String university_na = dataSnapshot.child("UniversityName").getValue().toString();
+                    String laddr = dataSnapshot.child("LocalAddress").getValue().toString();
+                    String cty = dataSnapshot.child("City").getValue().toString();
+                    String stt = dataSnapshot.child("State").getValue().toString();
+                    String pinc = dataSnapshot.child("PinCode").getValue().toString();
+                    String phone_no = dataSnapshot.child("PhoneNumber").getValue().toString();
+                    String Alt_phone_no = dataSnapshot.child("AlternativeNumber").getValue().toString();
+                    String email_address = dataSnapshot.child("EmailAddress").getValue().toString();
+                    String user_type = dataSnapshot.child("UserType").getValue().toString();
+                    un.setText(university_na);
+                    Laddr.setText(laddr);
+                    cityad.setText(cty);
+                    statead.setText(stt);
+                    pincodead.setText(pinc);
+                    ph.setText(phone_no);
+                    altph.setText(Alt_phone_no);
+                    utype.setText(user_type);
+                    eid.setText(email_address);
+                }
+                catch(Exception e)
+                {
+                    Toast.makeText(MyAccount.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+
             }
 
             @Override
