432adb6cf3207d66605be1713c045dce24a13e2d
Payel1998 <47295744+Payel1998@users.noreply.github.com>
2020-06-09 02:35:51 +0530

    MyEventsCreatedCompleted

--- app/src/main/java/com/example/myapplication/MyEventsCreatedTotal.java
+++ app/src/main/java/com/example/myapplication/MyEventsCreatedTotal.java
@@ -1,34 +1,48 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class MyEventsCreatedTotal extends AppCompatActivity {
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private EventCreatedTotalAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events_created_total);
 
-        ArrayList<ExampleItem> exampleList=new ArrayList<>();
-        exampleList.add(new ExampleItem("Event Held 1","Event Held 2"));
-        exampleList.add(new ExampleItem("Event Held 3","Event Held 4"));
-        exampleList.add(new ExampleItem("Event Held 5","Event Held 6"));
-
         mRecyclerView=findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager=new LinearLayoutManager(this);
-        mAdapter=new ExampleAdapter(exampleList);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        FirebaseUser user_id= FirebaseAuth.getInstance().getCurrentUser();
+        assert user_id != null;
+        String uid_user=user_id.getUid();
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("My Events Created").child(uid_user);
+
+        FirebaseRecyclerOptions<CreateEvent> options = new FirebaseRecyclerOptions.Builder<CreateEvent>().setQuery(ref, CreateEvent.class).build();
+
+        mAdapter = new EventCreatedTotalAdapter(MyEventsCreatedTotal.this, options);
+
         mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnClickListener(new EventCreatedTotalAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+
+            }
+        });
     }
 }
