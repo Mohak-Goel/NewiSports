81e21a7175073f56cd1c498cce4f5f575f9baf97
Mohak-Goel <1705417@kiit.ac.in>
2020-06-10 20:54:21 +0530

    Integration of app next level 3.1

--- app/src/main/java/com/example/myapplication/UploadResult.java
+++ app/src/main/java/com/example/myapplication/UploadResult.java
@@ -37,6 +37,7 @@ public class UploadResult extends AppCompatActivity {
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_result);
 
