450370d73a8e9acdc802759f83bda5c77632eb9f
NischayKG <1705421@kiit.ac.in>
2020-06-10 21:35:58 +0530

    Goel made me do this

--- app/src/main/java/com/example/myapplication/ParticipantList.java
+++ app/src/main/java/com/example/myapplication/ParticipantList.java
@@ -17,25 +17,28 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ParticipantList extends AppCompatActivity {
-    private FirebaseRecyclerOptions<model2> options2;
-    private FirebaseRecyclerAdapter<model2, MyviewHolder2> adapter;
+    private FirebaseRecyclerOptions<ParticipantUniversity> options2;
+    private FirebaseRecyclerAdapter<ParticipantUniversity, MyviewHolder2> adapter;
     private RecyclerView recyclerView;
+    private String edkey;
     DatabaseReference ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participant_list);
         Toast.makeText(ParticipantList.this, "Please Wait...", Toast.LENGTH_LONG).show();
-        ref= FirebaseDatabase.getInstance().getReference().child("Participant Details");
+
         recyclerView=findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        edkey=(String)getIntent().getStringExtra("ED Key");
+        ref= FirebaseDatabase.getInstance().getReference().child("Participant Details");
 
-        options2=new FirebaseRecyclerOptions.Builder<model2>().setQuery(ref,model2.class).build();
+        options2=new FirebaseRecyclerOptions.Builder<ParticipantUniversity>().setQuery(ref.orderByChild("eventid").equalTo(edkey),ParticipantUniversity.class).build();
 
-        adapter= new FirebaseRecyclerAdapter<model2, MyviewHolder2>(options2) {
+        adapter= new FirebaseRecyclerAdapter<ParticipantUniversity, MyviewHolder2>(options2) {
             @Override
-            protected void onBindViewHolder(@NonNull MyviewHolder2 holder, int position, @NonNull model2 model) {
+            protected void onBindViewHolder(@NonNull MyviewHolder2 holder, int position, @NonNull ParticipantUniversity model) {
                 holder.universityname.setText(model.getParticipantUnivName());
                 holder.universitycoachname.setText(model.getParticipantUnivCoachName());
 
