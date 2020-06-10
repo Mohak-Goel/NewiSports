3da9edcd9399ed01b26dbeb56bb2291b8ea401a7
NischayKG <1705421@kiit.ac.in>
2020-06-10 23:22:53 +0530

    Participants List Done Finally...

---
+++ app/src/main/java/com/example/myapplication/model2.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+public class model2 {
+    private String participantUnivName,participantUnivCoachName;
+
+    public model2() {
+    }
+
+    public model2(String participantUnivName, String participantUnivCoachName) {
+        this.participantUnivName = participantUnivName;
+        this.participantUnivCoachName = participantUnivCoachName;
+    }
+
+    public String getParticipantUnivName() {
+        return participantUnivName;
+    }
+
+    public void setParticipantUnivName(String participantUnivName) {
+        this.participantUnivName = participantUnivName;
+    }
+    public String getParticipantUnivCoachName() {
+        return participantUnivCoachName;
+    }
+
+    public void setParticipantUnivCoachName(String participantUnivCoachName) {
+        this.participantUnivCoachName = participantUnivCoachName;
+    }
+}
+
+
+
+
